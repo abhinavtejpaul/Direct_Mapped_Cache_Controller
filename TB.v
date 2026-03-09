1810d58d93febac93e13b586259d2df228534dc7
abhinavtejpaul <abhinavtejpaul28@gmail.com>
2026-03-10 02:41:13 +0530

    TB.v

--- TB.v
+++ TB.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.03.2026 05:38:41
-// Design Name: 
-// Module Name: TB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module TB(  );
 
