2dcc6d597432643c395552748223fefa4fbaa474
abhinavtejpaul <abhinavtejpaul28@gmail.com>
2026-03-10 02:40:37 +0530

    Data_Array.v

--- Data_Array.v
+++ Data_Array.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.03.2026 04:52:49
-// Design Name: 
-// Module Name: Data_Array
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Data_Array(
     input clk, // clk signal 
