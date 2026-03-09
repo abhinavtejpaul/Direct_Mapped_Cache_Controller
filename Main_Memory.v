125110adb07ccf9fede3312bbc039dfcf223b9c3
abhinavtejpaul <abhinavtejpaul28@gmail.com>
2026-03-10 02:40:53 +0530

    Main_Memory.v

--- Main_Memory.v
+++ Main_Memory.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09.03.2026 05:33:55
-// Design Name: 
-// Module Name: Main_Memory
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Main_Memory(
     input clk, //clk signal 
